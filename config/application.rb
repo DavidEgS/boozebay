44e884fe44b19c182068554ae649a68c7c408dc4
David Egerton <david.egertonsmith@gmail.com>
2021-03-20 18:57:03 +0000

    cheeky 2

--- config/application.rb
+++ config/application.rb
@@ -36,9 +36,3 @@ module Boozebay
   end
 end
 
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-     origins '*'
-     resource '*', :headers => :any, :methods => [:get, :post, :options]
-   end
-end
